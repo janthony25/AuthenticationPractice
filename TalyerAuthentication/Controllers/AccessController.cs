4a6929b3081239b4fe0185a6034b8597ff1ce4dc
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-04-03 10:40:22 +1300

    Add project files.

---
+++ TalyerAuthentication/Controllers/AccessController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using TalyerAuthentication.Models;
+
+namespace TalyerAuthentication.Controllers
+{
+    public class AccessController : Controller
+    {
+        public IActionResult Login()
+        {
+            ClaimsPrincipal claimUser = HttpContext.User;
+            if (claimUser.Identity.IsAuthenticated)
+                return RedirectToAction("Index", "Home");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(VMLogin modelLogin)
+        {
+            if(modelLogin.Username == "asd123" && 
+                modelLogin.Password == "asd")
+            {
+                List<Claim> claims = new List<Claim>()
+                {
+                    new Claim(ClaimTypes.NameIdentifier, modelLogin.Username),
+                    new Claim("OtherProperties", "Example Role")
+                };
+
+                ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims,
+                    CookieAuthenticationDefaults.AuthenticationScheme );
+
+                AuthenticationProperties properties = new AuthenticationProperties()
+                {
+                    AllowRefresh = true,
+                    IsPersistent = modelLogin.KeepLoggedIn
+                };
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(claimsIdentity), properties);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewData["ValidateMessage"] = "user not found";
+            return View();
+        }
+    }
+}
